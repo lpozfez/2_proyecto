20bf0ca557a382ff406a4791cfc1edda6e0b548a
Lourdes <lpozfer614@g.educaand.es>
2023-02-07 21:13:37 +0100

    Modificación de la entidad User. Se añaden los paquetes de seguridad.

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,93 +3,33 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-class User
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 50)]
-    private ?string $nombre = null;
-
-    #[ORM\Column(length: 150)]
-    private ?string $apellidos = null;
-
-    #[ORM\Column(length: 50)]
-    private ?string $nickname = null;
-
-    #[ORM\Column(length: 260)]
+    #[ORM\Column(length: 180, unique: true)]
     private ?string $email = null;
 
-    #[ORM\Column(length: 10)]
-    private ?string $contraseña = null;
+    #[ORM\Column]
+    private array $roles = [];
 
-    #[ORM\Column(length: 5)]
-    private ?string $rol = null;
-
-    #[ORM\Column(length: 32, nullable: true)]
-    private ?string $telegram = null;
-
-    #[ORM\Column(nullable: true)]
-    private ?int $puntos = null;
-
-    #[ORM\OneToMany(mappedBy: 'socio', targetEntity: Reserva::class)]
-    private Collection $reservas;
-
-    #[ORM\OneToMany(mappedBy: 'socio', targetEntity: Invitacion::class, orphanRemoval: true)]
-    private Collection $invitaciones;
-
-    public function __construct()
-    {
-        $this->reservas = new ArrayCollection();
-        $this->invitaciones = new ArrayCollection();
-    }
+    /**
+     * @var string The hashed password
+     */
+    #[ORM\Column]
+    private ?string $password = null;
 
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function getNombre(): ?string
-    {
-        return $this->nombre;
-    }
-
-    public function setNombre(string $nombre): self
-    {
-        $this->nombre = $nombre;
-
-        return $this;
-    }
-
-    public function getApellidos(): ?string
-    {
-        return $this->apellidos;
-    }
-
-    public function setApellidos(string $apellidos): self
-    {
-        $this->apellidos = $apellidos;
-
-        return $this;
-    }
-
-    public function getNickname(): ?string
-    {
-        return $this->nickname;
-    }
-
-    public function setNickname(string $nickname): self
-    {
-        $this->nickname = $nickname;
-
-        return $this;
     }
 
     public function getEmail(): ?string
@@ -104,111 +44,56 @@ class User
         return $this;
     }
 
-    public function getContraseña(): ?string
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
     {
-        return $this->contraseña;
+        return (string) $this->email;
     }
 
-    public function setContraseña(string $contraseña): self
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
     {
-        $this->contraseña = $contraseña;
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
 
-        return $this;
+        return array_unique($roles);
     }
 
-    public function getRol(): ?string
+    public function setRoles(array $roles): self
     {
-        return $this->rol;
-    }
-
-    public function setRol(string $rol): self
-    {
-        $this->rol = $rol;
-
-        return $this;
-    }
-
-    public function getTelegram(): ?string
-    {
-        return $this->telegram;
-    }
-
-    public function setTelegram(?string $telegram): self
-    {
-        $this->telegram = $telegram;
-
-        return $this;
-    }
-
-    public function getPuntos(): ?int
-    {
-        return $this->puntos;
-    }
-
-    public function setPuntos(?int $puntos): self
-    {
-        $this->puntos = $puntos;
+        $this->roles = $roles;
 
         return $this;
     }
 
     /**
-     * @return Collection<int, Reserva>
+     * @see PasswordAuthenticatedUserInterface
      */
-    public function getReservas(): Collection
+    public function getPassword(): string
     {
-        return $this->reservas;
+        return $this->password;
     }
 
-    public function addReserva(Reserva $reserva): self
+    public function setPassword(string $password): self
     {
-        if (!$this->reservas->contains($reserva)) {
-            $this->reservas->add($reserva);
-            $reserva->setSocio($this);
-        }
-
-        return $this;
-    }
-
-    public function removeReserva(Reserva $reserva): self
-    {
-        if ($this->reservas->removeElement($reserva)) {
-            // set the owning side to null (unless already changed)
-            if ($reserva->getSocio() === $this) {
-                $reserva->setSocio(null);
-            }
-        }
+        $this->password = $password;
 
         return $this;
     }
 
     /**
-     * @return Collection<int, Invitacion>
+     * @see UserInterface
      */
-    public function getInvitaciones(): Collection
+    public function eraseCredentials()
     {
-        return $this->invitaciones;
-    }
-
-    public function addInvitacione(Invitacion $invitacione): self
-    {
-        if (!$this->invitaciones->contains($invitacione)) {
-            $this->invitaciones->add($invitacione);
-            $invitacione->setSocio($this);
-        }
-
-        return $this;
-    }
-
-    public function removeInvitacione(Invitacion $invitacione): self
-    {
-        if ($this->invitaciones->removeElement($invitacione)) {
-            // set the owning side to null (unless already changed)
-            if ($invitacione->getSocio() === $this) {
-                $invitacione->setSocio(null);
-            }
-        }
-
-        return $this;
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
     }
 }
