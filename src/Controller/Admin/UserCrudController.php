e479c0ced630dbf35d3ddc8db29361110917ed92
Lourdes <lpozfer614@g.educaand.es>
2023-02-11 17:17:30 +0100

    Añadida plantilla de bootstrap y Easy Admin

---
+++ src/Controller/Admin/UserCrudController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+
+class UserCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return User::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('email'),
+            TextField::new('password'),
+            ChoiceField::new('roles')->setChoices(['ADMIN'=>'ROLE_ADMIN','USER'=>'ROLE_USER'])->allowMultipleChoices(),
+        ];
+    }
+}
